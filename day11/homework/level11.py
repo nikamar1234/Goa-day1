090602508da7b1d291292ec6d5a9f38b12676afc
nika <nikolozmariamidze91@gmail.com>
2024-08-15 19:38:44 +0400

    day11commit

---
+++ day11/homework/level11.py
@@ -0,0 +1,13 @@
+N1
+username = input("enter your name: ")
+myname = 'Nika'
+if username == myname:
+    print(True)
+else:
+    print(False)
+N2
+usernum = int(input('enter your number: '))
+if 0 < usernum < 10:
+    print(True)
+else:
+    print(False)
