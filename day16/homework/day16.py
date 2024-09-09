f30e124a097fc6d0863ab6390bf07349906a8cef
nika <nikolozmariamidze91@gmail.com>
2024-09-09 21:37:57 +0400

    day16 commit

---
+++ day16/homework/day16.py
@@ -0,0 +1,34 @@
+for i in range(21):
+    if i % 2 == 0:
+        print(i)
+
+for a in range(22):
+    if a % 2 != 0:
+        print(a)
+
+for b in range(10):
+    print(b/2)
+
+for c in range(10):
+    print(c * 2)
+
+for d in range(10):
+    print(d**2)
+
+for e in range(15):
+    print(e + 2)
+
+sum = 0
+for f in range(10):
+    sum = sum + f
+    
+print(sum)
+
+for g in range(0, 11, 2):
+    print(g)
+
+for h in range(1, 25, 3):
+    print(h)
+
+for j in range(10, 51, 2):
+    print(j)
